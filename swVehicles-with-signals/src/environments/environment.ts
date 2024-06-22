b6debda96b2134f3562a7e50d87bcee642b5e1ca
gbrun <gbrun@wideagency.com>
2024-06-22 15:26:51 +0200

    update to Angular 18.0.4

--- swVehicles-with-signals/src/environments/environment.ts
+++ swVehicles-with-signals/src/environments/environment.ts
@@ -1,5 +1,11 @@
 import { version } from '../../package.json';
+import { version as angularVersion } from '@angular/core/package.json';
+import { version as angularCliVersion } from '@angular/cli/package.json';
+import { version as typescriptVersion } from 'typescript/package.json';
 
 export const environment = {
   APP_VERSION: version,
+  ANGULAR_VERSION: angularVersion,
+  ANGULAR_CLI_VERSION: angularCliVersion,
+  TYPESCRIPT_VERSION: typescriptVersion,
 };
